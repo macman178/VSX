4e3efbf90f1f9002f6f0c8593f66cc04940ec722
VsyncCrypto <32690416+VsyncCrypto@users.noreply.github.com>
2018-04-20 14:24:47 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,8 +120,8 @@ public:
         nModifierUpdateBlock = 1;
 		
         nBlockEnforceSerialRange = 1; //Enforce serial range starting this block
-		nBlockEnforceInvalidUTXO = 350000;
-        nZerocoinStartTime = 1522972800; //Thursday, April 5, 2018 8:00:00 PM GMT-04:00
+		nBlockEnforceInvalidUTXO = 301000;
+        nZerocoinStartTime = 1524283210; //Saturday, April 21, 2018 12:00:10 AM GMT-04:00
 		nZerocoinStartHeight = 299282;
 		
 		const char* pszTimestamp = "Vsync 27-08-2017";
