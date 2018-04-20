a1b335d3303c909c022eda04600225e67f302703
VsyncCrypto <admin@vsync.pw>
2018-04-19 20:59:00 -0400

    fix typo

--- src/miner.cpp
+++ src/miner.cpp
@@ -33,7 +33,7 @@ using namespace std;
 
 //////////////////////////////////////////////////////////////////////////////
 //
-// VsyncMiner
+// VSXMiner
 //
 
 //
@@ -482,7 +482,7 @@ bool ProcessBlockFound(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
     {
         LOCK(cs_main);
         if (pblock->hashPrevBlock != chainActive.Tip()->GetBlockHash())
-            return error("VsyncMiner : generated block is stale");
+            return error("VSXMiner : generated block is stale");
     }
 
     // Remove key from key pool
@@ -497,7 +497,7 @@ bool ProcessBlockFound(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
     // Process this block the same as if we had received it from another node
     CValidationState state;
     if (!ProcessNewBlock(state, NULL, pblock))
-        return error("VsyncMiner : ProcessNewBlock, block not accepted");
+        return error("VSXMiner : ProcessNewBlock, block not accepted");
 
     for (CNode* node : vNodes) {
         node->PushInventory(CInv(MSG_BLOCK, pblock->GetHash()));
@@ -512,7 +512,7 @@ bool fGenerateBitcoins = false;
 
 void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
 {
-    LogPrintf("VsyncMiner started\n");
+    LogPrintf("VSXMiner started\n");
     SetThreadPriority(THREAD_PRIORITY_LOWEST);
     RenameThread("vsync-miner");
 
@@ -586,7 +586,7 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
             continue;
         }
 
-        LogPrintf("Running VsyncMiner with %u transactions in block (%u bytes)\n", pblock->vtx.size(),
+        LogPrintf("Running VSXMiner with %u transactions in block (%u bytes)\n", pblock->vtx.size(),
             ::GetSerializeSize(*pblock, SER_NETWORK, PROTOCOL_VERSION));
 
         //
